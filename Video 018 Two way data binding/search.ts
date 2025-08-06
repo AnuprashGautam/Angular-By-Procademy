77e917adcf94b2d4c884af79f8473b4fef000730
Anuprash <anuprashgautam@hotmail.com>
2025-08-06 23:59:02 +0530

    Updated files

---
+++ Video 018 Two way data binding/search.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  imports: [FormsModule],
+  templateUrl: './search.html',
+  styleUrl: './search.css'
+})
+export class Search {
+  searchText: string = "Womens Wear"
+
+  updateSearchText(event: any) {
+    this.searchText = event.target.value;
+  }
+}
+
+
